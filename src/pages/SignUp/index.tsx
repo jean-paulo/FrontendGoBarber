4f28fa3e0dea708a8fe901b76d7d119c4535e756
Jean Paulo <jeanprs@msn.com>
2020-07-18 15:07:55 -0300

    Validando cadastro com Yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+
+/** Importa tudo que tem dentro da lib de validação Yup na variável Yup */
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,9 +13,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void { //eslint-disable-line
-    console.log(data); //eslint-disable-line
-  }
+  const handleSubmit = useCallback(async (data: object) => { //eslint-disable-line
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'), // required significa que é obrigatório
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
