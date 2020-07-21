e320efb0ad21db547185586eabef933e46ec346f
Jean Paulo <jeanprs@msn.com>
2020-07-21 18:52:12 -0300

    User Storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,10 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object; //eslint-disable-line
+}
 
 interface SignInCredentials {
   email: string;
@@ -7,24 +12,43 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  name: string;
+  user: object; //eslint-disable-line
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    // Se ja existir no localstorage as informações, retorna como valor inical para esse estado
+    // os valores de token e user | o JSON.parse transforma de stringfy para objeto
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    // Se não encontrar um dos dois devolve um objeto vazio
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Jean', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
