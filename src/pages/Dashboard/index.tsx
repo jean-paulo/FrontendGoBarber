7082a5a991d4794b4a88b0925779e6dc40ce1dbb
jean-paulo <jeanprs@msn.com>
2020-08-18 22:05:34 -0300

    Listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Calendar,
+  Appointment,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
   console.log(user);
 
@@ -29,6 +41,85 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/51427898?s=460&u=e7facb40d695941e03de6c8fc0be8ab28d5fb3e6&v=4"
+                alt="Jean Paulo"
+              />
+              <strong>Jean Paulo</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong> Manhã </strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/51427898?s=460&u=e7facb40d695941e03de6c8fc0be8ab28d5fb3e6&v=4"
+                  alt="Jean Paulo"
+                />
+                <strong>Jean Paulo</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/51427898?s=460&u=e7facb40d695941e03de6c8fc0be8ab28d5fb3e6&v=4"
+                  alt="Jean Paulo"
+                />
+                <strong>Jean Paulo</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong> Tarde </strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/51427898?s=460&u=e7facb40d695941e03de6c8fc0be8ab28d5fb3e6&v=4"
+                  alt="Jean Paulo"
+                />
+                <strong>Jean Paulo</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
